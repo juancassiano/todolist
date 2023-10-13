1a0179a6c9108e62d81a7d3bb91d4652ad7a48b7
juancassiano <juancassiano@hotmail.com>
2023-10-13 19:21:55 -0300

    Vinculando ID de usuário nas tasks

--- src/main/java/br/com/juancassiano/todolist/task/TaskController.java
+++ src/main/java/br/com/juancassiano/todolist/task/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.juancassiano.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,7 +16,8 @@ public class TaskController {
   private TaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel){
+  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    taskModel.setIdUser((UUID) request.getAttribute("idUser"));
     return this.taskRepository.save(taskModel);
   }
   
--- src/main/java/br/com/juancassiano/todolist/task/filter/FilterTaskAuth.java
+++ src/main/java/br/com/juancassiano/todolist/task/filter/FilterTaskAuth.java
@@ -26,33 +26,34 @@ public class FilterTaskAuth extends OncePerRequestFilter{
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-        String authorization = request.getHeader("Authorization");
+        if(request.getServletPath().equals("/tasks/")){
+          String authorization = request.getHeader("Authorization");
 
-        String authEncoded = authorization.substring("Basic".length()).trim();
+          String authEncoded = authorization.substring("Basic".length()).trim();
 
-        byte[] authDecode =Base64.getDecoder().decode(authEncoded);
-        String authString = new String(authDecode);
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
+          byte[] authDecode =Base64.getDecoder().decode(authEncoded);
+          String authString = new String(authDecode);
+          String[] credentials = authString.split(":");
+          String username = credentials[0];
+          String password = credentials[1];
 
-        Optional<UserModel> user = this.userRepository.findByUsername(username);
-        if(user.isEmpty()) {
-          response.sendError(401);
-        }else{
-        
-        Result passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.get().getPassword());
+          Optional<UserModel> user = this.userRepository.findByUsername(username);
+          if(user.isEmpty()) {
+            response.sendError(401);
+          }else{
 
-        if(passwordVerify.verified){
+          Result passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.get().getPassword());
+
+          if(passwordVerify.verified){
+              request.setAttribute("idUser", user.get().getId());
+              filterChain.doFilter(request, response);
+          }else{
+            response.sendError(401);
+          }
+        }
+      }else{
         filterChain.doFilter(request, response);
-        }else{
-          response.sendError(401);
-        }
-
-        }
-
+      }
   }
 
- 
-  
 }
